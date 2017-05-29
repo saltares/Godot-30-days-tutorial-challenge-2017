a4a5b10b1cdd2fb13105f1ec95cab8e185b14aaf
David Saltares <david.saltares@gmail.com>
2017-05-29 14:29:18 +0100

    Complete tutorial 4

--- start/04-Custom camera with instant room transitions/Camera.gd
+++ start/04-Custom camera with instant room transitions/Camera.gd
@@ -2,19 +2,30 @@ extends Node2D
 
 onready var window_size = OS.get_window_size()
 onready var player = get_node("Player")
-onready var player_world_pos = get_player_world_pos()
+onready var player_grid_pos = get_player_grid_pos()
 
 
 func _ready():
 	var canvas_transform = get_viewport().get_canvas_transform()
-	canvas_transform[2] = player_world_pos * window_size
+	canvas_transform[2] = player_grid_pos * window_size
 	get_viewport().set_canvas_transform(canvas_transform)
 
 
-func get_player_world_pos():
-	# Converts the player position to the map he's on. Returns the map id/coordinates as Vector2
-	return Vector2()
+func get_player_grid_pos():
+	var pos = player.get_pos() - player.size * 0.5
+	pos.x = floor(pos.x / window_size.x)
+	pos.y = floor(pos.y / window_size.y)
+	return pos
 
 
 func update_camera():
-	pass
+	var new_player_grid_pos = get_player_grid_pos()
+	var transform = Matrix32()
+	
+	if new_player_grid_pos != player_grid_pos:
+		player_grid_pos = new_player_grid_pos
+		transform = get_viewport().get_canvas_transform()
+		transform[2] = -player_grid_pos * window_size
+		get_viewport().set_canvas_transform(transform)
+	
+	return transform
