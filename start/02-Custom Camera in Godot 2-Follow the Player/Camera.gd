4b7e4f10323b95882219abd95eec9651a56a62b2
David Saltares <david.saltares@gmail.com>
2017-05-29 12:32:39 +0100

    Complete tutorial 2

--- start/02-Custom Camera in Godot 2-Follow the Player/Camera.gd
+++ start/02-Custom Camera in Godot 2-Follow the Player/Camera.gd
@@ -3,12 +3,19 @@ extends Node
 
 onready var screen_size = Vector2(Globals.get("display/width"), Globals.get("display/height"))
 onready var player = get_node("Player")
+onready var last_player_pos = player.get_pos()
 
 func _ready():
 	var canvas_transform = get_viewport().get_canvas_transform()
-	canvas_transform[2] = player.get_pos() - screen_size / 2
+	canvas_transform[2] = last_player_pos - screen_size / 2
 	get_viewport().set_canvas_transform(canvas_transform)
 
 
 func update_camera():
-	print('moved!')
+	var player_offset = last_player_pos - player.get_pos()
+	last_player_pos = player.get_pos()
+	
+	var canvas_transform = get_viewport().get_canvas_transform()
+	canvas_transform[2] += player_offset
+	get_viewport().set_canvas_transform(canvas_transform)
+	
