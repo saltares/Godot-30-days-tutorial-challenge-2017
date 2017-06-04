30f730ca3b589d6c3d8d7164d305e6bce92a1a60
David Saltares <david.saltares@gmail.com>
2017-06-04 18:23:02 +0100

    Complete grid tutorials

--- start/08-Basic grid system/Grid.gd
+++ start/08-Basic grid system/Grid.gd
@@ -1,26 +1,67 @@
 extends TileMap
 
-var tile_size
-var half_tile_size
+var tile_size = get_cell_size()
+var half_tile_size = tile_size / 2
 
-var grid_size
+enum ENTITY_TYPES {PLAYER, OBSTACLE, COLLECTIBLE}
+
+var grid_size = Vector2(16, 16)
 var grid = []
 
-# onready var Obstacle = preload("res://Obstacle.tscn")
+onready var Obstacle = preload("res://Obstacle.tscn")
 
 
 func _ready():
-	# 1. Create the grid Array
-	# 2. Create obstacles
-	pass
+	_initialize_grid()
+	_initialize_player()
+	_create_obstables()
 
 
-func is_cell_vacant():
-	# Return true if the cell is vacant, else false
-	pass
+func is_cell_vacant(pos, direction):
+	var grid_pos = world_to_map(pos) + direction
+
+	if grid_pos.x >= grid_size.x or grid_pos.x < 0:
+		return false
+	if grid_pos.y >= grid_size.y or grid_pos.y < 0:
+		return false
+
+	return grid[grid_pos.x][grid_pos.y] == null
 
 
-func update_child_pos(child, new_pos, direction):
-	# Move a child to a new position in the grid Array
-	# Returns the new target world position of the child
-	pass
+func update_child_pos(child_node):
+	var grid_pos = world_to_map(child_node.get_pos())
+	print(grid_pos)
+	grid[grid_pos.x][grid_pos.y] = null
+
+	var new_grid_pos = grid_pos + child_node.direction
+	grid[new_grid_pos.x][new_grid_pos.y] = child_node.type
+
+	return map_to_world(new_grid_pos) + half_tile_size
+
+func _initialize_grid():
+	for x in range(grid_size.x):
+		grid.append([])
+		for y in range(grid_size.y):
+			grid[x].append(null)
+
+func _initialize_player():
+	var player = get_node("Player")
+	var start_pos = update_child_pos(player)
+	player.set_pos(start_pos)
+
+func _create_obstables():
+	var positions = []
+
+	for n in range(5):
+		var grid_pos = Vector2(
+			randi() % int(grid_size.x),
+			randi() % int(grid_size.y)
+		)
+		if not grid_pos in positions:
+			positions.append(grid_pos)
+
+	for pos in positions:
+		var new_obstacle = Obstacle.instance()
+		new_obstacle.set_pos(map_to_world(pos) + half_tile_size)
+		grid[pos.x][pos.y] = OBSTACLE
+		add_child(new_obstacle)
--- start/08-Basic grid system/Player.gd
+++ start/08-Basic grid system/Player.gd
@@ -6,35 +6,60 @@ const TOP = Vector2(0, -1)
 const RIGHT = Vector2(1, 0)
 const DOWN = Vector2(0, 1)
 const LEFT = Vector2(-1, 0)
+var type
+var grid
 
 var speed = 0
 const MAX_SPEED = 400
 
 var velocity = Vector2()
+var is_moving = false
+var target_pos = Vector2()
+var target_dir = Vector2()
 
 func _ready():
+	grid = get_parent()
+	type = grid.PLAYER
 	set_fixed_process(true)
 
 
 func _fixed_process(delta):
-	var is_moving = Input.is_action_pressed("move_up") or Input.is_action_pressed("move_right") or Input.is_action_pressed("move_down") or Input.is_action_pressed("move_left")
-	
 	direction = Vector2()
-	if is_moving:
+
+	if Input.is_action_pressed("move_up"):
+		direction = TOP
+	elif Input.is_action_pressed("move_down"):
+		direction = DOWN
+	elif Input.is_action_pressed("move_left"):
+		direction = LEFT
+	elif Input.is_action_pressed("move_right"):
+		direction = RIGHT
+
+	if direction.length_squared() > 0:
+		pass
+
+	if not is_moving and direction.length_squared() > 0:
+		target_dir = direction
+		if grid.is_cell_vacant(get_pos(), target_dir):
+			target_pos = grid.update_child_pos(self)
+			is_moving = true
+	elif is_moving:
 		speed = MAX_SPEED
-		
-		if Input.is_action_pressed("move_up"):
-			direction = TOP
-		elif Input.is_action_pressed("move_down"):
-			direction = DOWN
-		elif Input.is_action_pressed("move_left"):
-			direction = LEFT
-		elif Input.is_action_pressed("move_right"):
-			direction = RIGHT
-	else:
-		speed = 0
-	
-	velocity = speed * direction.normalized() * delta
-	
-	move(velocity)
-	
+		velocity = speed * target_dir * delta
+
+		var pos = get_pos()
+		var distance_to_target = Vector2(
+			abs(target_pos.x - pos.x),
+			abs(target_pos.y - pos.y)
+		)
+
+		if abs(velocity.x) > distance_to_target.x:
+			velocity.x = distance_to_target.x * target_dir.x
+			is_moving = false
+
+		if abs(velocity.y) > distance_to_target.y:
+			velocity.y = distance_to_target.y * target_dir.y
+			is_moving = false
+
+		move(velocity)
+
